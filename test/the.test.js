890f9acfaea4ca37727c0664cad7d17e15e4beb5
Evan Cobb <cobb.evan@gmail.com>
2018-10-16 22:11:38 -0500

    parses the graph

--- test/the.test.js
+++ test/the.test.js
@@ -1,33 +1,24 @@
-const graphs = [
-  `L O X P
-  O X O O
-  O O X O
-  O O O O`,
-]
-
-const outputs = [
-  `(0,0) L
-  (0,1) O
-  (0,2) O
-  (0,3) O
-  (1,3) O
-  (2,3) O
-  (3,3) O
-  (3,2) O
-  (3,1) O
-  (3,0) P`,
-]
-
 const test = require('ava');
 const traveler = require('../traveler');
 
-test('they pass', t => {
-  for (const index in graphs) {
-    const graph = graphs[index]
-    const expected = outputs[index]
+test('sam example', t => {
+    const graph = `L O X P
+    O X O O
+    O O X O
+    O O O O`
+    const LX = 0, LY = 0
+    const expected = `(0,0) L
+    (0,1) O
+    (0,2) O
+    (0,3) O
+    (1,3) O
+    (2,3) O
+    (3,3) O
+    (3,2) O
+    (3,1) O
+    (3,0) P`
 
-    const actual = traveler(graph)
+    const actual = traveler(graph, LX, LY)
 
     t.is(expected, actual, graph)
-  }
 })
--- traveler.js
+++ traveler.js
@@ -1,1 +1,28 @@
-module.exports = () => {};
+const travel = (graphDef, x, y) => {
+  const graph = parseGraph(graphDef)
+  // return shortestPath
+  console.log(graph);
+}
+
+const parseGraph = graphDef => {
+  const outer = []
+
+  outer.push([])
+
+  for (const char of graphDef) {
+    switch(char) {
+      case 'L':
+      case 'O':
+      case 'P':
+      case 'X':
+        outer[outer.length - 1].push(char)
+        break;
+      case '\n':
+        outer.push([])
+        break;
+    }
+  }
+  return outer;
+}
+
+module.exports = travel;
